b002095910bb12eae49b347fc2ab04454f7ada45
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-06-21 14:49:05 +0330

    P2 - edit package name

--- app/src/main/java/com/example/final_test_01/ui/customer/CustomerFragment.kt
+++ app/src/main/java/com/example/final_test_01/ui/customer/CustomerFragment.kt
@@ -1,24 +1,14 @@
 package com.example.final_test_01.ui.customer
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import com.example.final_test_01.R
-import com.example.final_test_01.databinding.DetailDialogBinding
-import com.example.final_test_01.databinding.FragmentCartBinding
 import com.example.final_test_01.databinding.FragmentCustomerBinding
-import com.example.final_test_01.ui.dialog_detail_items.DetailDialogViewModel
-import com.example.final_test_01.ui.dialog_detail_items.DetailsDialogFragmentArgs
-import com.example.final_test_01.ui.dialog_detail_items.adapter.DetailItemsAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
