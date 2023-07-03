a7757e32900203da3de02d7bab02abfb456482b6
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-07-04 01:58:27 +0330

    P3 - add snack Extention fun

--- app/src/main/java/com/example/final_test_01/util/Extention.kt
+++ app/src/main/java/com/example/final_test_01/util/Extention.kt
@@ -1,8 +1,14 @@
 package com.example.final_test_01.util
 
+import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 
 fun ImageView.showImage(url: String) {
     Glide.with(this).load(url).into(this)
+}
+
+fun View.snack(text: String) {
+    Snackbar.make(this, text, Snackbar.LENGTH_LONG).show()
 }
