5d13cb73b31fe6527f2f4525a8054355fecf5e44
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-07-05 18:50:22 +0330

    P3 - ساخت فانکشن برای مرتب سازی قیمت ها

--- app/src/main/java/com/example/final_test_01/util/Extention.kt
+++ app/src/main/java/com/example/final_test_01/util/Extention.kt
@@ -4,6 +4,8 @@ import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
+import java.text.NumberFormat
+import java.util.Locale
 
 fun ImageView.showImage(url: String) {
     Glide.with(this).load(url).into(this)
@@ -11,4 +13,9 @@ fun ImageView.showImage(url: String) {
 
 fun View.snack(text: String) {
     Snackbar.make(this, text, Snackbar.LENGTH_LONG).show()
+}
+
+fun Double.formatPrice(): String {
+    val numberFormat = NumberFormat.getNumberInstance(Locale.US)
+    return numberFormat.format(this)
 }
