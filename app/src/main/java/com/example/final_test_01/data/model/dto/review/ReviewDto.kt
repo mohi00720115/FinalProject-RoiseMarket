fefa6f518c037ce59e1a468cac91bd832f22e4c4
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-07-07 11:59:25 +0330

    P3 - change ReviewDto.kt

--- app/src/main/java/com/example/final_test_01/data/model/dto/review/ReviewDto.kt
+++ app/src/main/java/com/example/final_test_01/data/model/dto/review/ReviewDto.kt
@@ -5,27 +5,27 @@ import com.google.gson.annotations.SerializedName
 
 data class ReviewDto(
     @SerializedName("date_created")
-    val dateCreated: String?,
-    @SerializedName("date_created_gmt")
-    val dateCreatedGmt: String?,
+    val dateCreated: String? = "",
+//    @SerializedName("date_created_gmt")
+//    val dateCreatedGmt: String? = "",
     @SerializedName("id")
-    val id: Int?,
-    @SerializedName("_links")
-    val links: Links?,
+    val reviewId: Int?=0,
+//    @SerializedName("_links")
+//    val links: Links?,
     @SerializedName("product_id")
-    val productId: Int?,
+    val productId: Int? = 0,
     @SerializedName("rating")
-    val rating: Int?,
+    val rating: Int? = 0,
     @SerializedName("review")
-    val review: String?,
+    var review: String? = "",
     @SerializedName("reviewer")
-    val reviewer: String?,
-    @SerializedName("reviewer_avatar_urls")
-    val reviewerAvatarUrls: ReviewerAvatarUrls?,
+    var reviewer: String? = "",
+//    @SerializedName("reviewer_avatar_urls")
+//    val reviewerAvatarUrls: ReviewerAvatarUrls?,
     @SerializedName("reviewer_email")
-    val reviewerEmail: String?,
-    @SerializedName("status")
-    val status: String?,
-    @SerializedName("verified")
-    val verified: Boolean?
+    var reviewerEmail: String? = "",
+//    @SerializedName("status")
+//    val status: String? = "",
+//    @SerializedName("verified")
+//    val verified: Boolean?
 )
